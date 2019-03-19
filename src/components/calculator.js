3384e9c0922de29c5ae703d3fe85067405f3e6be
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2019-03-19 11:38:05 -0500

    Add files via upload

---
+++ src/components/calculator.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import CalculatorTitle from './calculatorTitle.js';
+import OutputScreen from './outputScreen.js';
+import Button from './button.js';
+
+class Calculator extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            question: '',
+            answer: ''
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    
+    
+
+    render(){
+        return (
+            <div className="frame">
+                <CalculatorTitle value={"React Calculator"} />
+                <div className="mainCalc">
+                    <OutputScreen answer={this.state.answer} question={this.state.question} />
+                    <div className="button-row">
+                        <Button handleClick = {this.handleClick} label={'Clear'} />
+                        <Button handleClick = {this.handleClick} label={'Delete'} />
+                        <Button handleClick = {this.handleClick} label={'.'} />
+                        <Button handleClick = {this.handleClick} label={'/'} />
+                    </div>
+                    <div className="button-row">
+                        <Button handleClick = {this.handleClick} label={'7'} />
+                        <Button handleClick = {this.handleClick} label={'8'} />
+                        <Button handleClick = {this.handleClick} label={'9'} />
+                        <Button handleClick = {this.handleClick} label={'*'} />
+                    </div>
+                    <div className="button-row">
+                        <Button handleClick = {this.handleClick} label={'4'} />
+                        <Button handleClick = {this.handleClick} label={'5'} />
+                        <Button handleClick = {this.handleClick} label={'6'} />
+                        <Button handleClick = {this.handleClick} label={'-'} />
+                    </div>
+                    <div className="button-row">
+                        <Button handleClick = {this.handleClick} label={'1'} />
+                        <Button handleClick = {this.handleClick} label={'2'} />
+                        <Button handleClick = {this.handleClick} label={'3'} />
+                        <Button handleClick = {this.handleClick} label={'+'} />
+                    </div>
+                    <div className="button-row">
+                        <Button handleClick = {this.handleClick} label={'0'} />
+                        <Button handleClick = {this.handleClick} label={'='} />
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
+    handleClick(event){
+        const value = event.target.value;
+
+        switch (value) {
+            case '=': {
+                if(this.state.question!==''){
+                    let ans = '';
+                        try {
+                            ans = eval(this.state.question);
+                        }
+                        catch(err) {
+                            this.setState({answer: "Math Error"});
+                        }
+                        if(ans===undefined){
+                            this.setState({answer: "Math Error"})
+                        } else {
+                            this.setState({answer: ans, question: ''});
+                            
+                        }
+                       
+                }
+                break;
+            }
+            case 'Clear': {
+                this.setState({question: '', answer: ''});
+                break;
+            }
+            case 'Delete': {
+                let str = this.state.question;
+                str = str.substr(0, str.length-1);
+                this.setState({question: str});
+                break;
+            }
+            default: {
+               this.setState({ question: this.state.question += value}) 
+                break;
+            }
+        }
+       
+    }
+}
+
+export default Calculator;
